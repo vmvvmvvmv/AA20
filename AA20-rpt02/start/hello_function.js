51f41cca86a8916e2fee754087f8997783f2da1c
vmvvmvvmv <100752566+vmvvmvvmv@users.noreply.github.com>
2022-09-08 16:30:00 +0900

    Add files via upload

---
+++ AA20-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what) {
+    console.log("Hello" + what + "!")
+}
+
+hello("AA20");
+hello("vmvvmvvmv, 임태형");
---
+++ AA20-rpt02/start/index_AA20.js
@@ -0,0 +1,5 @@
+var myinfo = require('./AA20info');
+
+myinfo("AA20", "mvmmvmmvm", "010-2971-1057");
+
+myinfo("AA55", "Comsi", "010-1234-5678");
